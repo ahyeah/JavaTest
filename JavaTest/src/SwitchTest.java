a82585f4343b762478532d6db8dc3968d41403a4
djc <514932631@qq.com>
2018-01-02 21:40:45 +0800

    s

---
+++ JavaTest/src/SwitchTest.java
@@ -0,0 +1,11 @@
+public class SwitchTest {
+    public void tes(){
+int b=1;
+        int a=0;
+        switch(a){
+            case 0: {a=b;break;}
+            case 1: {a++; break;}
+            default:a++;
+        }
+    }
+}
--- JavaTest/src/Testxor.java
+++ JavaTest/src/Testxor.java
@@ -1,6 +1,25 @@
 public class Testxor {
+
+
     public static void main(String args[]){
-        System.out.println(3^4);
+
+        String s1="test";
+        String s2=new String("test");
+        String s4=new String("test");
+        String s5="test";
+        String s3="te"+"st";
+        char data[] = {'t', 'e', 's','t'};
+        String str = new String(data);
+        System.out.println(s1==s2);
+        System.out.println(s2==s3);
+        System.out.println(s1==s3);
+        System.out.println(s4==s2);
+        System.out.println(s1==s5);
+
+        System.out.println(s1==str);
+        System.out.println(s2==str);
     }
 
+
 }
+
